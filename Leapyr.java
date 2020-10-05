f7579e88bf7315ac894949211a82ac2bc42db6bb
Harshad1999 <sonawaneharshad1999@gmail.com>
2020-10-06 00:00:18 +0530

    updated

--- Leapyr.java
+++ Leapyr.java
@@ -1,21 +1,39 @@
 import java.util.*;
 
 public class Leapyr {
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter any Year:");
         int year = sc.nextInt();
         sc.close();
+        Boolean isleapyr = false;
 
-        if (year%4==0) {
-            System.out.println(year + " is A Leap yr");
-            
-        }
-        else{
-            System.out.println(year+" is not a Leap yr");
+        if (year % 4 == 0) {
+
+            if (year % 100 == 0) {
+
+                if (year % 400 == 0) {
+                    isleapyr = true;
+
+                } else {
+                    isleapyr = false;
+                }
+
+            } else {
+                isleapyr = true;
+
+            }
+
+        } else {
+            isleapyr = false;
+
         }
 
-        
+        if (isleapyr == true)
+            System.out.println(year + " is a Leap Year.");
+        else
+            System.out.println(year + " is not a Leap Year.");
     }
+
 }
