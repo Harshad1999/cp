672532a92663fda47bfbb1a8f43560d199946247
Harshad1999 <sonawaneharshad1999@gmail.com>
2020-10-13 13:27:02 +0530

    fibo

---
+++ fibonacci.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class fibonacci {
+
+    static int fib(int n) {
+        if (n <= 1)
+            return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String args[]) {
+        int n = 10;
+        System.out.println(fib(n));
+    }
+}
